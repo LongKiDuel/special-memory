537017828c750493ed6b0c9626dd32714c9ba12e
LongKiDuel <96907398+LongKiDuel@users.noreply.github.com>
2023-09-12 16:46:53 +0800

    improve update-form-template.sh

--- scripts/update-form-template.sh
+++ scripts/update-form-template.sh
@@ -1,3 +1,19 @@
-git remote add template https://github.com/LongKiDuel/special-memory.git 
-git fetch --all  
-git merge template/main  --allow-unrelated-histories
+#!/bin/bash
+
+# Define the remote and its URL
+remote_name="template"
+remote_url="https://github.com/LongKiDuel/special-memory.git"
+
+# Check if the remote already exists
+if ! git remote | grep -q "$remote_name"; then
+    # Add the remote if it doesn't exist
+    git remote add "$remote_name" "$remote_url"
+else
+    echo "Remote '$remote_name' already exists."
+fi
+
+# Fetch from all remotes
+git fetch --all
+
+# Merge the branch, allowing unrelated histories
+git merge "$remote_name/main" --allow-unrelated-histories
