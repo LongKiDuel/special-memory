a4f0fa8b9369bc769e9933285bc0902829e268ad
John Doe <johndoe@example.com~>
2023-09-15 11:53:47 +0800

    add a build log.

--- include/Cmake.h
+++ include/Cmake.h
@@ -13,12 +13,17 @@ public:
   METHOD_LIST_BEGIN
   // use METHOD_ADD to add your custom processing function here;
   METHOD_ADD(CMake::build, "/build",{Get,Post,Options}); // path is /api/v1/CMake/build
+  METHOD_ADD(CMake::build_log, "/build-log",{Get,Post,Options}); // path is /api/v1/CMake/build
   METHOD_LIST_END
   // your declaration of processing function maybe like this:
   void build(const HttpRequestPtr &req,
-             std::function<void(const HttpResponsePtr &)> &&callback) const;
+             std::function<void(const HttpResponsePtr &)> &&callback)  ;
+  void build_log(const HttpRequestPtr &req,
+                 std::function<void(const HttpResponsePtr &)> &&callback) const;
 
-public:
+private:
+std::string buffer_;
+bool finished_{};
 };
 } // namespace v1
 } // namespace api
--- src/Cmake.cpp
+++ src/Cmake.cpp
@@ -1,9 +1,13 @@
 #include "Cmake.h"
 #include <cstdlib>
+#include <drogon/HttpResponse.h>
 #include <drogon/utils/FunctionTraits.h>
 #include <format>
+#include <functional>
 namespace {
-std::string executeCommand(const char *cmd) {
+std::string executeCommand(
+    const char *cmd,
+    std::function<void(std::string)> stream_callback = [](auto) {}) {
   std::array<char, 128> buffer;
   std::string result;
 
@@ -17,6 +21,7 @@ std::string executeCommand(const char *cmd) {
   while (!feof(pipe)) {
     if (fgets(buffer.data(), buffer.size(), pipe) != nullptr) {
       result += buffer.data();
+      stream_callback(buffer.data());
     }
   }
 
@@ -27,18 +32,41 @@ std::string executeCommand(const char *cmd) {
 } // namespace
 void api::v1::CMake::build(
     const HttpRequestPtr &req,
-    std::function<void(const HttpResponsePtr &)> &&callback) const {
+    std::function<void(const HttpResponsePtr &)> &&callback) {
   auto p = req->getParameter("path");
   if (p.empty()) {
     callback(HttpResponse::newNotFoundResponse());
     return;
   }
-  auto cmd = std::format("cmake -B {}/build -S {} && cmake --build {}/build 2>&1", p,
-                         p, p, p);
+  auto cmd = std::format(
+      "cmake -B {}/build -S {} && cmake --build {}/build 2>&1", p, p, p, p);
   std::cout << cmd << "\n";
-  auto result = executeCommand(cmd.c_str());
+
   auto r = HttpResponse::newHttpResponse();
-  r->setBody(result);
+  r->setBody("id:1");
+  r->addHeader("Refresh", "1;url=build-log?id=1"); // url=xxx
+  r->setContentTypeCode(ContentType::CT_TEXT_PLAIN);
+  callback(r);
+  finished_ = false;
+  buffer_.clear();
+  std::thread t {
+    [=,this] {
+      auto result = executeCommand(
+          cmd.c_str(), [this](auto stream_input) { buffer_ += stream_input; });
+      finished_ = true;
+    }
+  };
+  t.detach();
+}
+void api::v1::CMake::build_log(
+    const HttpRequestPtr &req,
+    std::function<void(const HttpResponsePtr &)> &&callback) const {
+  auto r = HttpResponse::newHttpResponse();
+  auto id = req->getParameter("id");
+  r->setBody(buffer_);
+  if (!finished_) {
+    r->addHeader("Refresh", "1;"); // url=xxx
+  }
   r->setContentTypeCode(ContentType::CT_TEXT_PLAIN);
   callback(r);
 }
