825b209987cc3c31e9ff6007e26c6c83887e9cbd
John Doe <johndoe@example.com~>
2023-09-19 16:37:03 +0800

    accept path as arg.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include "load_model.h"
 #include "report_model.h"
 #include <spdlog/spdlog.h>
-int main() {
-  auto model = load_model("/home/nanami/Downloads/lucy.obj").value();
+int main(int argc, char **argv) {
+  std::string path = argc > 1 ? argv[1] : "/home/nanami/Downloads/lucy.obj";
+  auto model = load_model(path).value();
 
   SPDLOG_INFO(report_model_all(model));
 }
