0459f7a01943668ef38c98fc32c3db2f97844aad
John Doe <johndoe@example.com~>
2023-10-23 15:29:27 +0800

    add world coord.

--- main.cpp
+++ main.cpp
@@ -71,20 +71,103 @@ void table() {
   ImGui::EndTable();
 }
 void paint() {
+
+  // world coord
+  // paint coord
+  // screen coord
   struct Canvas_context {
     ImVec2 canvas_offset_{};
     bool draw_grid_{true};
     float grid_size_{32.f};
+    float scale_factor_{1.f};
+
+    ImVec2 canvas_size_in_screen_;
+    ImVec2 canvas_offset_in_screen_;
+
+    ImVec2 get_position() const { return canvas_offset_in_screen_; }
+
+    ImVec2 get_canvas_size() const { return canvas_size_in_screen_; }
+    ImVec2 get_paint_size() const { return get_canvas_size() / scale_factor_; }
+    ImVec2 get_scanvas_min_position() const { return canvas_offset_in_screen_; }
+    ImVec2 get_canvas_max_position() const {
+      return get_canvas_size() + get_scanvas_min_position();
+    }
+    ImVec2 get_paint_min_position() const {
+      return get_paint_center() - get_paint_size() / 2;
+    }
+    ImVec2 get_paint_max_position() const {
+      return get_paint_center() + get_paint_size() / 2;
+    }
+    // normalized range is [-1,1]
+    ImVec2 paint_position_normalize(ImVec2 paint_position) {
+      return ((paint_position - get_paint_center()) / get_paint_size()) * 2;
+    }
+    ImVec2 paint_position_de_normalize(ImVec2 normalized_pos) {
+      return normalized_pos / 2 * get_paint_size() + get_paint_center();
+    }
+    ImVec2 paint_position_to_screen(ImVec2 paint_position) {
+      return normalized_position_to_screen(
+          paint_position_normalize(paint_position));
+    }
+    ImVec2 normalized_position_to_screen(ImVec2 position) {
+      return canvas_offset_in_screen_ +
+             (position / 2 + ImVec2(0.5f, 0.5f)) * get_canvas_size();
+    }
+
+    ImVec2 screen_position_normalize(ImVec2 screen_position) {
+      screen_position -= canvas_offset_in_screen_;
+      screen_position /= get_canvas_size();
+      screen_position *= 2;
+      screen_position -= ImVec2{1, 1};
+      return screen_position;
+    }
+    ImVec2 screen_position_de_normalize(ImVec2 screen_position) {
+      screen_position += ImVec2{1, 1};
+      screen_position /= 2;
+      screen_position *= get_canvas_size();
+      screen_position += canvas_offset_in_screen_;
+      return screen_position;
+    }
+    ImVec2 world_position_normalize(ImVec2 world_position) {
+      auto paint_position = world_position - canvas_offset_;
+      return paint_position_normalize(paint_position);
+    }
+    ImVec2 world_position_de_normalize(ImVec2 ndc) {
+      return paint_position_de_normalize(ndc) + canvas_offset_;
+    }
+
+    ImVec2 get_paint_center() const { return canvas_offset_; }
+
+    void begin_frame() {
+      canvas_size_in_screen_ = ImGui::GetContentRegionAvail();
+      canvas_offset_in_screen_ = ImGui::GetCursorScreenPos();
+    }
+    void end_frame() {}
   };
   static Canvas_context context;
+
+  const ImVec2 prev_size = context.get_paint_size();
   ImGui::Text("offset: %f %f", context.canvas_offset_.x,
               context.canvas_offset_.y);
+  const ImVec2 center = context.get_paint_center();
+  ImGui::Text("center: %f %f", center.x, center.y);
+  ImGui::DragFloat("scale", &context.scale_factor_, 0.01, 0.01);
+  if (context.scale_factor_ <= 0) {
+    context.scale_factor_ = 0.01;
+  }
+#define XX(fun) ImGui::Text(#fun ": %f %f", context.fun.x, context.fun.y)
+  XX(paint_position_normalize(center));
+  XX(get_paint_min_position());
+  XX(get_paint_max_position());
+  XX(paint_position_normalize(context.get_paint_min_position()));
+  XX(paint_position_normalize(context.get_paint_max_position()));
+  context.begin_frame();
 
-  const ImVec2 offset = ImGui::GetCursorScreenPos();
-  const ImVec2 size = ImGui::GetContentRegionAvail();
+  const ImVec2 size = context.get_canvas_size();
+  const ImVec2 offset = context.get_position();
 
-  const ImVec2 min_position = offset;
-  const ImVec2 max_postion = {offset.x + size.x, offset.y + size.y};
+  const ImVec2 min_position = context.get_scanvas_min_position();
+  const ImVec2 max_postion = context.get_canvas_max_position();
 
   auto draw_list = ImGui::GetWindowDrawList();
   draw_list->AddRectFilled(min_position, max_postion,
@@ -102,8 +185,8 @@ void paint() {
                       min_position.y + context.canvas_offset_.y};
 
   const auto &io = ImGui::GetIO();
-  const ImVec2 mouse_position_in_canvas{io.MousePos.x - origin.x,
-                                        io.MousePos.y - origin.y};
+  ImVec2 mouse_position_in_canvas{io.MousePos.x - origin.x,
+                                  io.MousePos.y - origin.y};
 
   if (is_active && ImGui::IsMouseDragging(ImGuiMouseButton_Middle)) {
     context.canvas_offset_.x += io.MouseDelta.x;
@@ -113,7 +196,7 @@ void paint() {
   draw_list->PushClipRect(min_position, max_postion, true);
 
   if (context.draw_grid_) {
-    const auto grid_size = context.grid_size_;
+    const auto grid_size = context.grid_size_ * context.scale_factor_;
     const auto grid_color = IM_COL32(150, 150, 150, 100);
 
     auto draw_line = [&](float x1, float x2, float y1, float y2) {
@@ -133,8 +216,8 @@ void paint() {
                  std::to_string(vec.y) + ")";
         };
 
-        append_text("mouse in cavnas", mouse_in_canvas);
-        append_text("mouse in cavnas world", mouse_in_canvas_world);
+        // append_text("mouse in cavnas", mouse_in_canvas);
+        // append_text("mouse in cavnas world", mouse_in_canvas_world);
 
         draw_list->AddText(ImVec2(x1, y1), -1, str.c_str());
       }
@@ -175,7 +258,26 @@ void paint() {
       draw_list->AddRectFilled(grid_to_screen(gid),
                                grid_to_screen(gid + ImVec2{1, 1}), -1);
     }
-    draw_list->AddCircleFilled(canvas_pos_to_screen(mouse_position_in_canvas), 10, IM_COL32(0, 0, 0, 255));
+    draw_list->AddCircleFilled(canvas_pos_to_screen(mouse_position_in_canvas),
+                               10, IM_COL32(0, 0, 0, 255));
+    draw_list->AddCircleFilled(
+        context.paint_position_to_screen(context.get_paint_center()), 10,
+        IM_COL32(255, 0, 0, 255));
+    draw_list->AddCircleFilled(
+        context.normalized_position_to_screen(
+            context.screen_position_normalize(io.MousePos)),
+        10, IM_COL32(0, 255, 0, 255));
+
+    auto world_origin = context.normalized_position_to_screen(
+        context.world_position_normalize(ImVec2{0, 0}));
+    draw_list->AddCircleFilled(world_origin, 10, IM_COL32(255, 255, 255, 255));
+
+    draw_list->AddLine(world_origin - ImVec2{5000, 0},
+                       world_origin + ImVec2{5000, 0}, IM_COL32(155, 0, 0, 255),
+                       3);
+    draw_list->AddLine(world_origin - ImVec2{0, 5000},
+                       world_origin + ImVec2{0, 5000}, IM_COL32(0, 155, 0, 255),
+                       3);
   }
 
   draw_list->PopClipRect();
