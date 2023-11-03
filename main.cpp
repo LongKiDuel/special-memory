857ea2d6dfebdbf564068973451eedbd8c2268ee
John Doe <johndoe@example.com~>
2023-11-03 15:45:41 +0800

    fix multi recive.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,18 @@
 #include <asio.hpp>
 #include <asio/registered_buffer.hpp>
+#include <asio/socket_base.hpp>
+#include <functional>
 #include <iostream>
 #include <memory>
 #include <string>
-
-void send_broadcast(asio::io_context &io_context) {
+auto &getSocket(asio::io_context &io_context) {
   static asio::ip::udp::socket socket(
-      io_context, asio::ip::udp::endpoint(asio::ip::udp::v4(), 0));
+      io_context, asio::ip::udp::endpoint(asio::ip::udp::v4(), 12312));
+  return socket;
+}
+void send_broadcast(asio::io_context &io_context) {
+  static auto &socket = getSocket(io_context);
+  socket.set_option(asio::socket_base::reuse_address(true));
   socket.set_option(asio::socket_base::broadcast(true));
 
   const std::string message = "Network discovery probe";
@@ -17,23 +23,36 @@ void send_broadcast(asio::io_context &io_context) {
   // lifetime.
   static auto receive_buffer = std::make_shared<std::vector<char>>(1024);
 
-  socket.async_send_to(
-      asio::buffer(message), receiver_endpoint,
-      [&](std::error_code /*ec*/, std::size_t /*bytes_sent*/) {
-        // Start the receive operation after the send completes
-        socket.async_receive_from(
-            asio::buffer(*receive_buffer), receiver_endpoint,
-            [&io_context](const std::error_code &ec, std::size_t bytes_recvd) {
-              if (!ec && bytes_recvd > 0) {
-                std::cout << "Received response from: " << receiver_endpoint
-                          << "\n";
-                std::cout << "Message: "
-                          << std::string(receive_buffer->begin(),
-                                         receive_buffer->begin() + bytes_recvd)
-                          << "\n";
-              }
-            });
-      });
+  socket.async_send_to(asio::buffer(message), receiver_endpoint,
+                       [&](std::error_code /*ec*/, std::size_t /*bytes_sent*/) {
+
+                       });
+}
+
+void recive_broadcast_echo(asio::io_context &io_context) {
+  static auto &socket = getSocket(io_context);
+  static asio::ip::udp::endpoint receiver_endpoint(
+      asio::ip::address_v4::broadcast(), 8888);
+
+  // Prepare a buffer for the incoming message. Use a shared_ptr to manage the
+  // lifetime.
+
+  auto receive_buffer = std::make_shared<std::vector<char>>(1024);
+
+  auto recive_handle = [receive_buffer, &io_context](const std::error_code &ec,
+                                                     std::size_t bytes_recvd) {
+    if (!ec && bytes_recvd > 0) {
+      std::cout << "Received response from: " << receiver_endpoint << "\n";
+      std::cout << "Message: "
+                << std::string(receive_buffer->begin(),
+                               receive_buffer->begin() + bytes_recvd)
+                << "\n";
+    }
+    recive_broadcast_echo(io_context);
+  };
+  std::cout << "start reciving: \n";
+  socket.async_receive_from(asio::buffer(*receive_buffer), receiver_endpoint,
+                            recive_handle);
 }
 
 void listen_for_responses(asio::io_context &io_context) {
@@ -74,6 +93,7 @@ int main(int argc, char **argv) {
   signals.async_wait([&](const std::error_code &, int) { io_context.stop(); });
   if (argc > 1) {
     // Send a broadcast message
+    recive_broadcast_echo(io_context);
     send_broadcast(io_context);
   } else {
     // Listen for any responses
