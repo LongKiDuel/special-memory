6b25c24d1396e36f53b41fa054b0489e12f24eb3
John Doe <johndoe@example.com~>
2023-12-19 09:34:25 +0800

    create basic for implot.

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <cmath>
 #include <functional>
 #include <imgui.h>
+#include <implot.h>
 #include <memory>
 #include <string>
 #include <vector>
@@ -17,10 +18,25 @@ public:
   std::function<void()> draw_call_;
 };
 
-void paint() {}
+void paint() {
+  std::function<float(float)> func = [](float x) { return x; };
+  if (ImPlot::BeginPlot("My Plot")) {
+    std::vector<float> x_axis{};
+    std::vector<float> y_axis{};
+    const int count = 1000;
+    for (int i = 0; i <= count; i++) {
+      x_axis.push_back(i * 1.f / (count));
+      y_axis.push_back(func(x_axis.back()));
+    }
+    ImPlot::PlotLine("My Line Plot", x_axis.data(), y_axis.data(), count);
+
+    ImPlot::EndPlot();
+  }
+}
 int main() {
   auto app = ImGuiX::create_vulkan_app();
 
+  ImPlot::CreateContext();
   // app->add_window(std::make_shared<Window_slot>("Table", table));
   app->add_window(std::make_shared<Window_slot>("Draw", paint));
 
