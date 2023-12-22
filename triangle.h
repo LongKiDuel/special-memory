ffd6c197059a581d6f30347f7228dc9f754a0df1
John Doe <johndoe@example.com~>
2023-12-22 10:39:32 +0800

    add triangle stream.

---
+++ triangle.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <array>
+#include <functional>
+#include <glm/ext/vector_float3.hpp>
+namespace data_stream {
+struct Triangle_ref {
+  std::array<glm::vec3 *, 3> a;
+};
+struct Triangle_stream {
+  bool write(const Triangle_ref &tri) { return callback_(tri); }
+  bool write(float *a, float *b, float *c) {
+    return write(Triangle_ref{
+        reinterpret_cast<glm::vec3 *>(a),
+        reinterpret_cast<glm::vec3 *>(b),
+        reinterpret_cast<glm::vec3 *>(c),
+    });
+  }
+  std::function<bool(const Triangle_ref &tri)> callback_{}; // return false to
+                                                            // break.
+};
+} // namespace data_stream
