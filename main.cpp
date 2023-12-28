e5295a2a203bbe6416339f4efb1729458e96ba22
John Doe <johndoe@example.com~>
2023-12-28 10:09:11 +0800

    add colorfun text.

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <algorithm>
 #include <filesystem>
 #include <optional>
+#include <string_view>
 #define IMGUI_DEFINE_MATH_OPERATORS
 ///
 #include "imguix/window.h"
@@ -15,6 +16,33 @@
 #include <memory>
 #include <string>
 #include <vector>
+
+void colorful_text(std::string_view str) {
+  auto rd = str.find("RED");
+  ImVec4 color = {1, 0, 0, 1};
+  if (rd == -1) {
+    ImGui::Text("%s", std::string{str}.c_str());
+  } else {
+    ImGui::Text("%s", std::string{str.substr(0, rd)}.c_str());
+    str = str.substr(rd + 3);
+    std::string_view red_part;
+    auto wd = str.find("WHITE");
+    if (wd == -1) {
+      red_part = str;
+    } else {
+      red_part = str.substr(0, wd);
+    }
+
+    ImGui::SameLine(0, 0);
+    ImGui::TextColored(color, "%s", std::string{red_part}.c_str());
+
+    if (wd != -1) {
+      str = str.substr(wd + 5);
+      ImGui::SameLine(0, 0);
+      colorful_text(str);
+    }
+  }
+}
 class Window_slot : public ImGuiX::Window {
 public:
   Window_slot(std::string title, std::function<void()> call)
@@ -52,14 +80,21 @@ ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
 인간이 폭정과 억압에
 Whereas disregard and contempt for human rights have resulted
   )**";
+  std::string color_text = R"**(
+The code in imgui.cpp embeds a copy of 'ProggyClean.ttf' (by Tristan Grimmer), a RED13WHITE pixels high, pixel-perfect font used by default. We embed it in the source code so you can use Dear ImGui without any file system access. ProggyClean does not scale smoothly, therefore it is recommended that you load your own file when using Dear ImGui in an application aiming to look nice and wanting to support multiple resolutions.
+  )**";
   ImGuiX::Font_builder builder;
   builder.add(content);
+  builder.add(color_text);
   builder.add('?');
 
   static std::vector<std::string> fonts = get_font_files();
   static std::string *selected = fonts.data();
   static std::string input;
   builder.add(input);
+
+  builder.add("MHP:/");
+  builder.add(ImGui::GetIO().Fonts->GetGlyphRangesDefault());
   if (fonts.empty()) {
     ImGui::Text("No fonts");
     return;
@@ -90,6 +125,19 @@ Whereas disregard and contempt for human rights have resulted
         [info] { manage.build(info); });
   }
   ImGui::Text("%s", content.c_str());
+  colorful_text(color_text);
+
+  ImGui::Text("HP: ");
+  ImGui::PushStyleColor(ImGuiCol_Text, {0, 1, 0, 1});
+  ImGui::SameLine();
+  ImGui::Text("1523");
+  ImGui::PopStyleColor();
+
+  ImGui::Text("MP: ");
+  ImGui::PushStyleColor(ImGuiCol_Text, {0.2, 0.5, 0.9, 1});
+  ImGui::SameLine();
+  ImGui::Text("89/100");
+  ImGui::PopStyleColor();
 
   ImGui::InputText("Input", &input);
   ImGui::TextWrapped("%s", input.c_str());
